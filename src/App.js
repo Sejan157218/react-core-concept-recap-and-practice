684e59bd3d176e04989d0472236fc0a38f6fb478
sejan-mahmud <sejan157218@gmail.com>
2021-09-19 03:03:37 +0600

    create jsx and add css

--- src/App.js
+++ src/App.js
@@ -2,24 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ArticleWithJSX></ArticleWithJSX>
+      <Blog heading = 'Heading 1' author = 'Sejan'></Blog>
+      <Blog heading = 'Heading 2' author = 'Sajal'></Blog>
+      <Blog heading = 'Heading 3' author = 'ASH'></Blog>
     </div>
   );
+}
+function ArticleWithJSX(){
+  const h2Style = {
+    color : "#c7f2ff",
+    backgroundColor : '#293752',
+    borderRadius: '10px',
+  }
+  return (
+    <article className="blog">
+      <h2 style={h2Style}>This is Article Tag</h2> 
+    <p style={{color : "#c7f2ff",backgroundColor : '#293752',borderRadius: '10px',}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dignissimos rerum, repellat numquam sapiente fugiat, veniam quis sint quos, quia quaerat! Consequuntur inventore cupiditate unde eaque voluptas illum hic esse!</p>
+    </article>
+  )
+}
+
+function Blog(props){
+  return (
+    <div className="blog">
+      <h2>Heading  : {props.heading}</h2>
+      <h2>Author : {props.author}</h2>
+    </div>
+  )
 }
 
 export default App;
